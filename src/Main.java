cd7556302b1c0911a1e5ffaa539283d104965967
Ilenko Evgeniy <Ilenko2012@gmail.com>
2022-09-05 09:00:48 +0500

    add  mod ; files

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         int initialamount = 200;
-        int addAmount = 900;
+        int addAmount = 300;
         int bonus = 0;
-        if (addAmount > 1000);
+        if (addAmount > 1000) 
         bonus = addAmount / 100;
-
         int finalAccount = initialamount + addAmount + bonus;
         System.out.println("На счете:" + finalAccount);
         System.out.println("Бонус:" + bonus);
+
     }
 }
